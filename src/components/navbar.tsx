0db96b57d930b85782c09304b9485de59c85d668
JMOrbegoso <dev@jmorbegoso.com>
2021-02-08 16:51:17 -0500

    Updated NavBar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,21 +2,20 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Link from 'next/link';
+import { WEB_NAME } from '../lib/constants';
 import LanguagePicker from './language-picker';
 import useTranslation from 'next-translate/useTranslation';
 import TranslationResource from '../enums/translationResource';
 
-type Props = {
-  title: string;
-};
+type Props = {};
 
-const NavBar = ({ title }: Props) => {
+const NavBar = ({}: Props) => {
   const { t, lang } = useTranslation('common');
 
   return (
     <Navbar bg="primary" variant="dark" fixed="top" expand="lg">
       <Link href="/#" passHref>
-        <Navbar.Brand>{title}</Navbar.Brand>
+        <Navbar.Brand>{WEB_NAME}</Navbar.Brand>
       </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
