0a0d4d2cf316fbaa3e924e7711d9f2e952367092
JMOrbegoso <dev@jmorbegoso.com>
2021-02-07 22:12:49 -0500

    Created AboutMe component

---
+++ src/components/about-me.tsx
@@ -0,0 +1,21 @@
+import Container from './container';
+import markdownStyles from '../components/markdown-styles.module.css';
+
+type Props = {
+  authorContent: string;
+};
+
+const AboutMe = ({ authorContent }: Props) => {
+  return (
+    <Container>
+      <div className="max-w-2xl mx-auto text-justify">
+        <div
+          className={markdownStyles['markdown']}
+          dangerouslySetInnerHTML={{ __html: authorContent }}
+        />
+      </div>
+    </Container>
+  );
+};
+
+export default AboutMe;
