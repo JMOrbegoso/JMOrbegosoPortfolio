fad9e76bb84cf67d8ead44ad940936f81fa27feb
JMOrbegoso <dev@jmorbegoso.com>
2021-02-07 21:26:18 -0500

    Updated ProjectPreview component

--- src/components/project-preview.tsx
+++ src/components/project-preview.tsx
@@ -1,48 +1,36 @@
-import DateFormatter from './date-formatter';
 import Link from 'next/link';
 import { Tag } from '../enums/tag';
 import ProjectTags from './project-tags';
-import CoverImage from './cover-image';
+import { Card } from 'react-bootstrap';
 
 type Props = {
   title: string;
-  coverImage: string;
-  date: string;
   excerpt: string;
+  coverImage: string;
   slug: string;
-  content: string;
   tags: Tag[];
 };
 
-const ProjectPreview = ({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  slug,
-  content,
-  tags,
-}: Props) => {
+const ProjectPreview = ({ title, excerpt, coverImage, slug, tags }: Props) => {
   return (
-    <div className="my-3">
-      <h3 className="text-3xl mb-3 leading-snug text-center">
+    <Card className="m-2">
+      <Link as={`/projects/${slug}`} href="/projects/[slug]">
+        <a aria-label={title}>
+          <Card.Img variant="top" src={coverImage} />
+        </a>
+      </Link>
+      <Card.Body>
         <Link as={`/projects/${slug}`} href="/projects/[slug]">
-          <a className="hover:underline">{title}</a>
+          <a className="hover:underline">
+            <Card.Title className="text-center">{title}</Card.Title>
+          </a>
         </Link>
-      </h3>
-      <div className="text-lg mb-4">
-        <div className="row">
-          <div className="col-6 text-left">
-            <DateFormatter dateString={date} />
-          </div>
-        </div>
-      </div>
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <p className="text-lg leading-relaxed mb-4 text-justify">{excerpt}</p>
-      <ProjectTags tags={tags} />
-    </div>
+        <Card.Text className="text-justify">{excerpt}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <ProjectTags tags={tags} />
+      </Card.Footer>
+    </Card>
   );
 };
 
