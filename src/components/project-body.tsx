608b1157162bc31d348976057a197656a781573c
JMOrbegoso <dev@jmorbegoso.com>
2021-02-12 08:44:59 -0500

    Updated ProjectBody component Changed the default target property to _blank.

--- src/components/project-body.tsx
+++ src/components/project-body.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import markdownStyles from './markdown-styles.module.css';
 
 type Props = {
@@ -6,12 +7,17 @@ type Props = {
 
 const ProjectBody = ({ content }: Props) => {
   return (
-    <div className="max-w-2xl mx-auto text-justify">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
+    <>
+      <Head>
+        <base target="_blank" />
+      </Head>
+      <div className="max-w-2xl mx-auto text-justify">
+        <div
+          className={markdownStyles['markdown']}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
+    </>
   );
 };
 
